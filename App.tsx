65e05abcd46532ce58b90f11ddcf3d8eca106d36
Barbora Cervenkova <cervenkovab@gmail.com>
2022-04-17 15:25:09 +0200

    add prettier and fix

--- App.tsx
+++ App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, useColorScheme} from 'react-native';
+import { SafeAreaView, ScrollView, Text, useColorScheme } from 'react-native';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? 'red' : 'white',
-  };
-  console.log('kvak');
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Text>kvakdsdkdkdkdkdk</Text>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? 'red' : 'white',
+    };
+    console.log('kvak');
+    const kvak = 'ddd';
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+                <Text>{kvak}</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 export default App;
--- README.md
+++ README.md
@@ -1,29 +1,39 @@
 # simple-sample-react-native-app
+
 ## setup
+
 [https://reactnative.dev/docs/environment-setup](https://reactnative.dev/docs/environment-setup)
+
 - node v 16.4.2
 
 ## run
+
 bundler
+
 ```
 npx react-native start
 ```
 
 Android
+
 ```
 npx react-native run-android
 ```
 
 ios
+
 ```
 npx react-native run-ios
 ```
 
 ### dev
+
 run Metro bundler
+
 ```
  npx react-native start
 ```
+
 ```
 REACT_DEBUGGER="open -g 'rndebugger://set-debugger-loc?port=8081' ||" npx react-native run-ios
 ```
@@ -31,6 +41,7 @@ REACT_DEBUGGER="open -g 'rndebugger://set-debugger-loc?port=8081' ||" npx react-
 ### troubleshooting
 
 clear all and do fresh build:
+
 ```
 watchman watch-del-all && rm -rf node_modules/ && yarn cache clean && yarn install && yarn start --reset-cache
 ```

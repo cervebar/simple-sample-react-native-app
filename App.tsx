734e4aae5a75f186ed20730f3ba42c1b13205ba2
Barbora Cervenkova <cervenkovab@gmail.com>
2022-04-17 19:47:08 +0200

    redux toolikt

--- App.tsx
+++ App.tsx
@@ -4,17 +4,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DetailScreen } from './src/screens/DetailScreen';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { RouteDetail, RouteHome } from './src/hooks/navigationParams';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name={RouteHome} component={HomeScreen} />
-                <Stack.Screen name={RouteDetail} component={DetailScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name={RouteHome} component={HomeScreen} />
+                    <Stack.Screen name={RouteDetail} component={DetailScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
     );
 };
 export default App;

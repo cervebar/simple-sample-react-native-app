89fbede5fa5a294b1bb07aeefffd072ecfd33e78
Barbora Cervenkova <cervenkovab@gmail.com>
2022-04-17 21:08:32 +0200

    persist storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,17 +2,27 @@ import { combineReducers, configureStore, MiddlewareArray } from '@reduxjs/toolk
 import messageReducer from './message';
 import favoriteReducer from './favorites';
 import { useDispatch } from 'react-redux';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer, persistStore } from 'redux-persist';
 const rootReducer = combineReducers({
     message: messageReducer,
     favorite: favoriteReducer,
 });
 
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: new MiddlewareArray(), //TODO .concat(middleware)
 });
 export type RootState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>(); // Export a hook that can be reused to resolve types
+
+export const persistor = persistStore(store);

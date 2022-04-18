bbdf42ff4fff5f34b4a23d2e324641f73eede8ec
Barbora Cervenkova <cervenkovab@gmail.com>
2022-04-18 18:41:00 +0200

    search on input

--- src/components/SearchResultMovies.tsx
+++ src/components/SearchResultMovies.tsx
@@ -1,21 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { ActivityIndicator, Button, FlatList, TextInput, View } from 'react-native';
 import { FetchContext } from '../fetch/FetchProvider';
 import { ItemT } from '../types/ItemT';
 import { ListMovieItem } from './ListMovieItem';
 
-export type SearchResultMoviesProps = {
-    searchQuery: string;
-};
-
-export const SearchResultMovies = ({ searchQuery }: SearchResultMoviesProps) => {
-    const [isLoading, setLoading] = useState(true);
+export const SearchResultMovies = () => {
+    const [isLoading, setLoading] = useState(false);
     const [data, setData] = useState<ItemT[]>([]);
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [page, setPage] = useState<number>(1);
     const { strategy } = useContext(FetchContext);
 
-    const getMovies = async () => {
+    const fetchData = async () => {
         try {
-            const response = await strategy.fetchMovies(searchQuery);
+            const response = await strategy.fetchMovies(searchQuery, page);
             setData(response);
         } catch (error) {
             console.error(error);
@@ -24,21 +22,49 @@ export const SearchResultMovies = ({ searchQuery }: SearchResultMoviesProps) => 
         }
     };
 
-    useEffect(() => {
-        getMovies();
-    }, []);
+    const triggerSearch = async (searchQueryInput: string) => {
+        if (searchQueryInput.length < 3) {
+            return; // start fetching after first 3 characters
+        }
+        setPage(1); //reset to start
+        setLoading(true);
+        setSearchQuery(searchQueryInput);
+        await fetchData();
+    };
+
+    const nextPage = async () => {
+        // TODO max pages from fetchresult
+        setPage(page + 1);
+        await fetchData();
+    };
+    const prevPage = async () => {
+        setPage(Math.max(1, page - 1));
+        await fetchData();
+    };
 
     return (
-        <View style={{ flex: 1, padding: 24 }}>
-            {isLoading ? (
-                <ActivityIndicator />
-            ) : (
-                <FlatList
-                    data={data}
-                    keyExtractor={({ id }) => id}
-                    renderItem={({ item }) => <ListMovieItem data={item} />}
-                />
-            )}
-        </View>
+        <>
+            <TextInput
+                style={{ height: 40 }}
+                placeholder="start searching your movie"
+                onChangeText={text => {
+                    triggerSearch(text);
+                }}
+            />
+            {/*TODO prevent click when in boucdaries */}
+            <Button title="Next page" onPress={nextPage} />
+            <Button title="Prev page" onPress={prevPage} />
+            <View style={{ flex: 1, padding: 24 }}>
+                {isLoading ? (
+                    <ActivityIndicator />
+                ) : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }) => id}
+                        renderItem={({ item }) => <ListMovieItem data={item} />}
+                    />
+                )}
+            </View>
+        </>
     );
 };
